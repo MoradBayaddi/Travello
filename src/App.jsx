b69f4b59a85e1e56836dacffae0170033f9c881b
Morad Bayaddi <mourad201630@gmail.com>
2024-01-11 01:07:57 +0100

    adding rest of code

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import Navbar from './components/navbar'
 import Hero from './components/Hero'
-// import Features from './components/Features'
-// import Destinations from './components/Destinations'
-// import About from './components/About'
-// import Contact from './components/Contact'
-// import Footer from './components/Footer'
+import Features from './components/Features'
+import Destinations from './components/Destinations'
+import About from './components/About'
+import Contact from './components/Contact'
+import Footer from './components/Footer'
 
 function App() {
   return (
@@ -13,11 +13,11 @@ function App() {
     <>
       <Navbar />
       <main>
-        {/* <div id="hero">
+        <div id="hero">
           <Hero/>
-        </div> */}
+        </div>
         
-        {/* <div id="features">
+        <div id="features">
           <Features/>
         </div>
 
@@ -35,7 +35,7 @@ function App() {
 
         <div id="footer">
           <Footer/>
-        </div> */}
+        </div>
       </main>
     </>
   )
